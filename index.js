4cc7ce6548215b5ed34ebaadc972217d3c2e62bb
ELIAS <eliaskipkirui1@student.moringaschool.com>
2024-12-16 17:18:58 +0300

    complete lab1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code in this file!
+// Define the current user
+const currentUser = 'Grace Hopper';
+
+// Create the welcome message using a template literal
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Generate an excited welcome message using .toUpperCase()
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Create a short greeting with only the first initial of the currentUser
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
